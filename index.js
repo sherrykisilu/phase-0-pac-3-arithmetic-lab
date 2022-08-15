17bb1875174fa419b4259dcf691d814476433b0e
sherry <sherry.kisilu@studebnt.moringaschool.com>
2022-08-15 17:12:03 +0300

    assignment compelete

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b){
+    return a+b;
+}
+console.log(add(1,80));
+function subtract(a,b){
+    return a-b;
+}
+console.log(subtract(60-40));
+function multiply(a,b){
+    return a*b;
+}
+console.log(multiply(2*3.4));
+function divide(a,b){
+    return a/b;   
+}
+console.log(divide(5.0/2.5));
+number=n 
+function increment(n){
+    return ++n;
+}
+console.log('616');
+function decrement(n){
+    return --n;
+}
+console.log('616');
+function makeInt(n){
+   return parseInt(n,10);      
+}
+console.log(makeInt(n));
+function makeint(string){
+   return parseFloat(n,10);
+}
+console.log(makeInt(n));
+function preserveDecimal(n){
+    return parseFloat(n,10);
+}
+console.log(preserveDecimal(n));
